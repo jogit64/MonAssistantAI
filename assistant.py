853748aef6ed8bfd051e53fb272061c9cd1a3bf9
jogit64 <usersfdc18@gmail.com>
2024-04-03 08:26:44 +0200

    Version heroku online avec session key

--- assistant.py
+++ assistant.py
@@ -1,44 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from openai import OpenAI
-# from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+app.secret_key = 'assistant-ai-1a-urrugne-64122'  # Définissez une clé secrète pour les sessions
 
-# Charge les variables d'environnement du fichier .env
-# load_dotenv()
-
-# Initialisation du client OpenAI avec votre clé API
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-# Variable globale pour stocker l'historique des messages
-global_message_history = []
 
 @app.route('/')
 def home():
+    session['message_history'] = []  # Réinitialise l'historique pour chaque nouvelle session
     return render_template('index.html')
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
     question = request.form['question']
     
-    # Ajouter la nouvelle question à l'historique des messages
-    global global_message_history
-    global_message_history.append({"role": "user", "content": question})
+    # Récupère l'historique des messages de la session actuelle
+    message_history = session.get('message_history', [])
     
-    # Envoie la requête avec l'historique des messages
+    # Ajoute la nouvelle question à l'historique des messages de la session
+    message_history.append({"role": "user", "content": question})
+    
+    # Envoie la requête avec l'historique des messages de la session
     chat_completion = client.chat.completions.create(
-        messages=global_message_history,
+        messages=message_history,
         model="gpt-3.5-turbo",
     )
     
-    # Récupérer la réponse de l'API
+    # Récupère la réponse de l'API
     response_chatgpt = chat_completion.choices[0].message.content
     
-    # Ajouter la réponse à l'historique des messages
-    global_message_history.append({"role": "assistant", "content": response_chatgpt})
+    # Ajoute la réponse à l'historique des messages de la session
+    message_history.append({"role": "assistant", "content": response_chatgpt})
     
-    return render_template('index.html', messages=global_message_history)
+    # Sauvegarde l'historique mis à jour dans la session
+    session['message_history'] = message_history
+    
+    return render_template('index.html', messages=message_history)
 
 if __name__ == '__main__':
     app.run(debug=True)
